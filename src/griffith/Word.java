7434df5fb35953547b5201c7966b2398b2cb4fae
Anaissuh13 <hussainah2407@gmail.com>
2025-02-19 08:50:58 +0000

    Created and updated the Word.java and WordTest.java classes witht the stub methods and updated the Word doc with the screenshots of the same.

---
+++ src/griffith/Word.java
@@ -0,0 +1,21 @@
+package griffith;
+
+public class Word {
+	private char[] letters;
+
+    public Word(char[] letters) {
+        this.letters = letters;
+    }
+
+    public boolean contains(char symbol) {
+        return false; // Placeholder
+    }
+
+    public int length() {
+        return 0; // Placeholder
+    }
+
+    public char[] getLetters() {
+        return letters; // Placeholder
+    }
+}
---
+++ src/griffithTest/WordTest.java
@@ -0,0 +1,33 @@
+package griffithTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class WordTest {
+
+	@Test
+    public void testContains() {
+        // Placeholder test
+    }
+
+    @Test
+    public void testLength() {
+        // Placeholder test
+    }
+
+    @Test
+    public void testNotNull() {
+        // Placeholder test
+    }
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
